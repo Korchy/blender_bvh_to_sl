ee95df32bc552d85ac77becab6b9ea576d904132
Nikita Akimov <korchiy@yandex.ru>
2021-04-03 23:26:21 +0300

    update

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ if "bpy" in locals():
 
 bl_info = {
     "name": "BVH to SL",
-    "author": "Campbell Barton",
+    "author": "Campbell Barton, Andrea Rugliancich, Nikita Akimov",
     "blender": (2, 83, 13),
     "location": "File > Export",
     "description": "Unofficial fork of the original Campbell Barton's add-on for exporting BVH to SL",
